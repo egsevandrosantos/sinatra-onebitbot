2615443fd29e022155b37038d1c91bb75063414f
Evandro Gabriel dos Santos <egs.evandro.santos@gmail.com>
2020-05-14 23:28:13 -0300

    ajuste help service

--- app/services/help_service.rb
+++ app/services/help_service.rb
@@ -1,18 +1,18 @@
 class HelpService
     def self.call
-        response = '*Meus comandos* \n\n'
-        response += 'help\n'
-        response += '`Lista de comandos que eu conheço`\n\n'
-        response += 'Adicione ao Faq\n'
-        response += '`Adiciona uma nova questão ao Faq`\n\n'
-        response += 'Remova Id\n'
-        response += '`Remove uma questão baseado no número identificador (Id) passado`\n\n'
-        response += 'O que você sabe sobre ?\n'
-        response += '`Pesquisa pela palavra informada (?) na lista de perguntas e respostas`\n\n'
-        response += 'Pesquise a hashtag ?\n'
-        response += '`Lista as perguntas e respostas com a hashtag informada (?)`\n\n'
-        response += 'Perguntas e Respostas\n'
-        response += '`Mostra a lista de perguntas e respostas`\n\n'
+        response = "*Meus comandos* \n\n"
+        response += "help\n"
+        response += "`Lista de comandos que eu conheço`\n\n"
+        response += "Adicione ao Faq\n"
+        response += "`Adiciona uma nova questão ao Faq`\n\n"
+        response += "Remova Id\n"
+        response += "`Remove uma questão baseado no número identificador (Id) passado`\n\n"
+        response += "O que você sabe sobre ?\n"
+        response += "`Pesquisa pela palavra informada (?) na lista de perguntas e respostas`\n\n"
+        response += "Pesquise a hashtag ?\n"
+        response += "`Lista as perguntas e respostas com a hashtag informada (?)`\n\n"
+        response += "Perguntas e Respostas\n"
+        response += "`Mostra a lista de perguntas e respostas`\n\n"
         response
     end
 end
